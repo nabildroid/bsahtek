7fb2e05b0a4bc36835ebda8706235774421a609a
nabildroid <pni20156789@gmail.com>
2023-07-22 09:50:11 +0100

    fix bug

--- uber_client/lib/screens/home.dart
+++ uber_client/lib/screens/home.dart
@@ -49,7 +49,7 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
       context.read<AppCubit>().recheckRunningOrder();
     }
 
-    super.didChangeAppLifecycleState(appLifecycleState);
+    super.didChangeAppLifecycleState(state);
   }
 
   @override
