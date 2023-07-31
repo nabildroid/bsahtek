c295fd9ab9f0724482dd6ab67b84fe5b2af4a096
nabildroid <pni20156789@gmail.com>
2023-07-31 15:29:03 +0100

    ux

--- uber_admin/src/app/(dashboard)/delivers/page.tsx
+++ uber_admin/src/app/(dashboard)/delivers/page.tsx
@@ -5,6 +5,8 @@ import { useAtom } from "jotai";
 import { useQuery } from "react-query";
 import * as Server from "@/local_repository/server";
 import Link from "next/link";
+import { useState } from "react";
+import Icons from "@/svgs";
 
 export default function Page() {
     const [user] = useAtom(userAtomAsync);
@@ -15,7 +17,9 @@ export default function Page() {
 
 
     return <div className="max-w-3xl mx-auto  grid sm:grid-cols-3 grid-cols-1 px-2 gap-6">
-        {data?.map((deliver, i) => <div className="bg-white group ring ring-stone-200 shadow rounded-lg overflow-hidden">
+        {data?.map((deliver, i) => <div className="bg-white group ring ring-stone-200 shadow rounded-lg overflow-hidden relative">
+
+            <DropDown id={deliver.id!} />
             <div className="p-2 text-center ">
                 <img className="w-20 mx-auto aspect-square rounded-full" src={deliver.photo} />
                 <h2 className="font-bold my-2">{deliver.name}</h2>
@@ -24,12 +28,49 @@ export default function Page() {
 
             </div>
 
-            <Link href={`/deliver_requests/${deliver.id}/review`} className="block text-center text-black font-bold group-hover:text-white group-hover:bg-black py-2 w-full">
-                view
+            <Link href={`/deliver_requests/${deliver.id}/details`} className="block text-center text-black font-bold group-hover:text-white group-hover:bg-black py-2 w-full">
+                View
             </Link>
 
         </div>
         )}
 
     </div>
+}
+
+function DropDown(params: { id: string }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggle() {
+        if (isOpen) {
+            setIsOpen(false);
+        } else {
+            setIsOpen(true);
+            setTimeout(() => {
+                setIsOpen(false);
+            }, 10000);
+        }
+
+    }
+
+    return <>
+        <button
+            onClick={toggle}
+            id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" className="absolute top-2 right-2 inline-flex items-center p-2 text-sm font-medium text-center rounded-lg text-black  hover:bg-black hover:text-white" type="button">
+            <Icons.MoreVertical className="w-5 h-5" />
+        </button>
+
+        <div id="dropdownDots" className={`absolute top-10 left-10 z-10  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 ${!isOpen ? "hidden" : ""}`}>
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
+                <li>
+                    <Link className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" href={`/deliver_request/${params.id}/review`}>Review</Link>
+
+                </li>
+
+            </ul>
+        </div>
+
+    </>
+
 }
--- uber_admin/src/app/(dashboard)/sellers/layout.tsx
+++ uber_admin/src/app/(dashboard)/sellers/layout.tsx
@@ -6,6 +6,7 @@ import { useAtom } from "jotai";
 import * as Server from "@/local_repository/server";
 import { useQuery } from "react-query"
 import Link from "next/link";
+import { useState } from "react";
 
 type Props = {
     children: React.ReactNode;
@@ -24,15 +25,17 @@ export default function Layout(props: Props) {
         <div className="grid grid-cols-1 px-2 sm:grid-cols-2 lg:grid-cols-4 gap-6  mt-8">
 
             {data?.map((bag, i) => <div key={bag.id} className=" group shadow-md bg-white ring ring-stone-200 rounded-lg overflow-hidden">
+
                 <div className="p-2" >
 
                     <img src={bag.photo} className="object-cover w-full rounded-lg h-16" />
 
                     <div className="-mt-16">
-                        <div className="flex items-center p-2">
+                        <div className="flex items-center p-2 relative">
                             <img src={bag.sellerPhoto} className="w-10 h-10 rounded-full object-cover ring ring-white" />
 
                             <div className="flex-1" />
+                            <DropDown id={bag.sellerID} />
 
                         </div>
                     </div>
@@ -43,8 +46,8 @@ export default function Layout(props: Props) {
                     </div>
 
                 </div>
-                <Link href={`/seller_requests/${bag.sellerID}/assign`} className="block text-center text-black font-bold group-hover:text-white  group-hover:bg-black py-2 w-full">
-                    Edit
+                <Link href={`/seller_requests/${bag.sellerID}/details`} className="block text-center text-black font-bold group-hover:text-white  group-hover:bg-black py-2 w-full">
+                    View
                 </Link>
 
             </div>
@@ -58,4 +61,44 @@ export default function Layout(props: Props) {
 
 
     </div>
+}
+
+
+
+
+function DropDown(params: { id: string }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggle() {
+        if (isOpen) {
+            setIsOpen(false);
+        } else {
+            setIsOpen(true);
+            setTimeout(() => {
+                setIsOpen(false);
+            }, 10000);
+        }
+
+    }
+
+    return <>
+        <button
+            onClick={toggle}
+            id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" className="inline-flex items-center p-2 text-sm font-medium text-center rounded-lg text-white bg-black/40 hover:bg-white hover:text-black" type="button">
+            <Icons.MoreVertical className="w-5 h-5" />
+        </button>
+
+        <div id="dropdownDots" className={`absolute top-10 left-10 z-10  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 ${!isOpen ? "hidden" : ""}`}>
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
+                <li>
+                    <Link className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" href={`/seller_request/${params.id}/assign`}>Edit</Link>
+
+                </li>
+
+            </ul>
+        </div>
+
+    </>
+
 }
