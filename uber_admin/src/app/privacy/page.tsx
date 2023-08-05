873cce5f053d185c5f6bd97d176b88719d2b0c57
nabildroid <pni20156789@gmail.com>
2023-08-05 21:10:12 +0100

    add privacy page

---
+++ uber_admin/src/app/privacy/page.tsx
@@ -0,0 +1,64 @@
+export default function Page() {
+    return (
+        <div className="bg-gray-100 min-h-screen py-12">
+            <div className="max-w-4xl mx-auto px-6 md:px-10">
+                <h1 className="text-3xl font-semibold mb-6">Privacy Policy</h1>
+
+                <div className="bg-white rounded-lg shadow-md p-6">
+                    <h2 className="text-xl font-semibold mb-4">Privacy Policy for Bsahtek</h2>
+                    <p className="text-gray-700">
+                        This Privacy Policy explains how Samok Co collects, uses, shares, and protects your personal information when you use the Bsahtek mobile application. By accessing or using the App, you consent to the collection and use of your personal information as described in this Policy. If you do not agree with this Policy, you must not use the App.  <br></br> Information We Collect <br></br> 1.1. Personal Information: We may collect personal information that you provide to us when you create an account, such as your name, email address, phone number, and payment information. <br></br> 1.2. Usage Information: We may collect information about your interactions with the App, including the features you use, the content you view, and your preferences. <br></br> 1.3. Location Information: With your consent, we may collect your precise location information through the App to provide location-based services and connect you with nearby Merchants.
+                    </p>
+
+
+                    <h2 className="text-xl font-semibold my-4">Use of Information</h2>
+                    <p className="text-gray-700">
+                        2.1. We use the collected information to:  <br></br>Provide and improve the App\'s functionality, features, and services.  <br></br>Process and fulfill your orders and transactions with the merchants.  <br></br>Communicate with you regarding your account, orders, and App updates. <br></br>Personalize your experience with the App and provide relevant recommendations.  <br></br>Analyze and monitor usage patterns and trends to enhance the App\'s performance.  <br></br>Detect, prevent, and address technical issues, fraud, or other unauthorized activities.
+                    </p>
+
+
+                    <h2 className="text-xl font-semibold my-4">Sharing of Information</h2>
+                    <p className="text-gray-700">
+                        3.1. We may share your personal information with third parties in the following circumstances:  <br></br>With Merchants to facilitate the purchase and delivery of surplus food. <br></br>With service providers who assist us in operating the App and providing related services. <br></br>With business partners and affiliates for marketing and promotional purposes, with your consent.  <br></br>In response to a valid legal request or to comply with applicable laws, regulations, or legal processes.  <br></br>To protect our rights, property, or safety, and that of our users or others.
+                    </p>
+
+
+                    <h2 className="text-xl font-semibold my-4">Data Security</h2>
+                    <p className="text-gray-700">
+                        4.1. We implement reasonable security measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic storage is completely secure, and we cannot guarantee absolute security.
+                    </p>
+
+
+                    <h2 className="text-xl font-semibold my-4">Third-Party Links and Services</h2>
+                    <p className="text-gray-700">
+                        5.1. The App may contain links to third-party websites or services that are not owned or controlled by us. This Policy does not apply to those third-party websites or services. We recommend reviewing the privacy policies of those third parties before providing any personal information.
+                    </p>
+
+
+                    <h2 className="text-xl font-semibold my-4">Children\'s Privacy</h2>
+                    <p className="text-gray-700">
+                        6.1. The App is not intended for use by individuals under the age of 18. We do not knowingly collect personal information from children under 18. If we become aware that we have inadvertently collected personal information from a child under 18, we will take steps to delete such information as soon as possible.
+                    </p>
+
+
+
+
+
+
+                    <h2 className="text-xl font-semibold my-4">Changes to this Policy</h2>
+                    <p className="text-gray-700">
+                        7.1. We may update this Policy from time to time. The updated Policy will be effective upon posting the revised version on the App. We encourage you to review this Policy periodically to stay informed about how we collect, use, and protect your information.
+                    </p>
+
+
+
+                    <h2 className="text-xl font-semibold my-4">Contact Us</h2>
+                    <p className="text-gray-700">
+                        8.1. If you have any questions, concerns, or requests regarding this Policy or our privacy practices, please contact us at contact information. <br></br>By using the App, you acknowledge that you have read and understood this Privacy Policy and consent to the collection, use, and sharing of your personal information as described herein.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    );
+};
