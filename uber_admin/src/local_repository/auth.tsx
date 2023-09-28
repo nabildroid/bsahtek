b99fc0d62bb24f9d6b7e18bdc4f6124e3d85a9e9
nabildroid <pni20156789@gmail.com>
2023-09-28 22:40:35 +0100

    fix bug

--- uber_admin/src/local_repository/auth.tsx
+++ uber_admin/src/local_repository/auth.tsx
@@ -18,7 +18,7 @@ export function ListenToAuth(onUser: (user: User) => void) {
       const data = await user.getIdTokenResult();
 
 
-      if (!user.email?.endsWith("@bshatek.net")) {
+      if (!user.email?.endsWith("@bsahtek.net")) {
         AuthClient.signOut();
         Cookies.remove("token");
         Cookies.remove("isLogged");
