d519962f03b60b6415237e37ade2a02cbbc1615c
nabildroid <pni20156789@gmail.com>
2024-03-23 13:16:35 +0100

    fix bug

--- uber_client/lib/repositories/server.dart
+++ uber_client/lib/repositories/server.dart
@@ -19,7 +19,7 @@ class Server {
   static late FirebaseFirestore firestore;
   static late FirebaseAuth auth;
   static Dio http = Dio(BaseOptions(
-    baseUrl: "http://192.168.0.105:3000/api/",
+    baseUrl: "https://bsahtek.net/api/",
   ));
 
   static Future<void> init() async {
